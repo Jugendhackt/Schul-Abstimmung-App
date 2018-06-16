55967a6cab647c27f7b2bdccc8983ee842ec1eab
Max Mustermann <m.mustermann@ebinf.eu>
2018-06-16 19:42:02 +0200

    PHP eingefügt

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<html>
-    <head>
+<?php
+
+	require("includes/global.inc.php");
+	$con = db_connect();
+	
+	$user = 2;
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
         <title>Schulstimme</title>
         <link href="design.css" type="text/css" rel="stylesheet">
     </head>
