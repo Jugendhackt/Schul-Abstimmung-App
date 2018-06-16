8eaacc383171839cd123494594ad6dd3aaccb2fb
eb informatik <erik@ebinf.eu>
2018-06-16 21:55:02 +0200

    Anpassungen

--- index.php
+++ index.php
@@ -11,11 +11,30 @@
 	<head>
 		<meta charset="utf-8">
         <title>Schulstimme</title>
-        <link href="design.css" type="text/css" rel="stylesheet">
+        <link href="design.css" type="css" rel="stylesheet"/>
+		<link href="idee.html" type="html" rel="important"/>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		<title>Home-Schulstimme</title>
     </head>
     <body>
         <div id="logo">
-            <p class="">Schulstimme</p>
-        </div>
+			<p class="titel"> 
+				Schulstimme
+			</p>
+		</div>
+		<div class="dropdown">
+        	<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         	 Menu
+        	</button>
+        	<div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+         		<button class="dropdown-item" type="button">Home</button>
+				<button class="dropdown-item" type="button">Idee erstellen</button>
+				<button class="dropdown-item" type="button">Ergebnisse ansehen</button>
+				<button class="dropdown-item" type="button">Abmelden</button>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     </body>
 </html>
