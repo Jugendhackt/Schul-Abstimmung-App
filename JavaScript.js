5bf903749392c022c0208aea95b27e7573fa1d73
Lena Rothbauer <linea@Lenas-MacBook-Pro.local>
2018-06-16 21:17:51 +0200

    dateien hinzugefügt:html, JavaScript Frontend

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Menu</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="home2.html">Home</a>
+      <a class="dropdown-item" href="idee.html">Idee erstellen</a>
+      <a class="dropdown-item" href="idee.html">Ergebnisse ansehen</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="idee.html">Abmelden</a>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</ul>
